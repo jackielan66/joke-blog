2350e57126be3f8c82201d96a8a0447a79298cee
jackie <1370672560@qq.com>
2025-05-05 16:36:13 +0800

    header rename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "@/components/Header";
+import Header from "@/components/Headerr";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
