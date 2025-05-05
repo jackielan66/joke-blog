8b600db21f32a864a0c7962d75eec449b707177d
jackie <1370672560@qq.com>
2025-05-05 11:59:31 +0800

    fix prodution issuse

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,8 +1,8 @@
-export default function LoadingSpinner({ text }) {
+export default function LoadingSpinner({  }) {
     return (
       <div className="flex items-center space-x-2 text-gray-500">
         <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-gray-400"></div>
-        <span>{text}</span>
+        <span>text</span>
       </div>
     )
   }
