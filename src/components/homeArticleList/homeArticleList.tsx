689772ba00a8b629e2bbdcc63eeb5bea883d20db
jackie <1370672560@qq.com>
2025-05-12 18:05:23 +0800

    Link replace a, fix prefetch issue

--- src/components/homeArticleList/homeArticleList.tsx
+++ src/components/homeArticleList/homeArticleList.tsx
@@ -30,8 +30,7 @@ const HomeArticleList = ({
     return (
         <div className="space-y-4 p-4">
             {articles.map((article, index) => (
-                <Link href={`/blog/${article.docid}`} key={index}
-                    prefetch={false}
+                <a href={`/blog/${article.docid}`} key={index}
                     className="flex flex-col md:flex-row items-center gap-4 p-4 rounded-lg shadow-md hover:cursor-pointer transition duration-300 ease-in-out bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
                     <div className="w-full md:w-1/4 h-40 relative">
@@ -43,7 +42,7 @@ const HomeArticleList = ({
                         <h2 className="text-xl font-semibold text-gray-900 dark:text-white">{article.title}</h2>
                         <p className="text-sm text-gray-600 dark:text-gray-400 mt-2">{article.ptime}</p>
                     </div>
-                </Link>
+                </a>
             ))}
         </div>
     )
